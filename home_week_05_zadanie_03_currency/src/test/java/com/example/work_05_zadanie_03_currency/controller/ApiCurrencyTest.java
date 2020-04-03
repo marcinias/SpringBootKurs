d49a27a344d53636373c865aa0c67fdf3bc8543c
marcinias <marcinias@sixfaces.pl>
2020-04-03 16:08:37 +0200

    add test unit

--- home_week_05_zadanie_03_currency/src/test/java/com/example/work_05_zadanie_03_currency/controller/ApiCurrencyTest.java
+++ home_week_05_zadanie_03_currency/src/test/java/com/example/work_05_zadanie_03_currency/controller/ApiCurrencyTest.java
@@ -6,12 +6,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.ui.Model;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
@@ -26,11 +24,8 @@ public class ApiCurrencyTest {
     @MockBean
     private ServiceCurrency serviceCurrency;
 
-
     @Test
     public void verifiesHomePageLoads() throws Exception {
-
-
 
         mockMvc.perform(MockMvcRequestBuilders.get("/check?usdInput=4.4"))
                 .andDo(print())
